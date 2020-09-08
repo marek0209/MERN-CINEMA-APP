2465090f173625f4796ec8f86a33eb595915255d
Marek Urbaniec <marek.urbaniec1@gmail.com>
2020-09-08 21:27:47 +0200

    Prepare structure for  client app
    Add components, directory for reducers, actions etc. Add cdn for materialize in index.html

---
+++ Client/src/components/auth/Register.js
@@ -0,0 +1,8 @@
+import React from "react";
+class Register extends React.Component {
+  render() {
+    return <h1>Register</h1>;
+  }
+}
+
+export default Register;
---
+++ Client/src/components/layout/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+class Navbar extends React.Component {
+  render() {
+    return <h1>Navbar</h1>;
+  }
+}
+
+export default Navbar;
