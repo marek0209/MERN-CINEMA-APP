5faad531483c7d6ad17c26e5625fc85f8a68d4f3
Marek Urbaniec <marek.urbaniec1@gmail.com>
2020-09-08 22:59:21 +0200

    Add Redux  functionalities for rooms from api
    Adds Redux actions, reducers for rooms, error reducer

---
+++ Client/src/actions/roomActions.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import {
+  FETCH_ROOMS_PENDING,
+  FETCH_ROOMS_SUCCESS,
+  FETCH_ROOMS_ERROR,
+  CREATE_ROOMS_SUCCESS,
+  CREATE_ROOMS_ERROR,
+} from "./types";
+
+export const createRoomsAction = (roomData, token) => (dispatch) => {
+  axios
+    .post("http://localhost:5000/api/rooms/", roomData, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    })
+    .then((res) => {
+      if (res.error) {
+        throw res.error;
+      } else {
+        dispatch(createRoomsSuccess(res.data));
+        return res.data;
+      }
+    })
+    .catch((err) => dispatch(createRoomsError(err)));
+};
+
+//Fetch rooms from API
+export function fetchRoomsAction() {
+  return (dispatch) => {
+    dispatch(fetchRoomsPending());
+    fetch("http://localhost:5000/api/rooms")
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          throw res.error;
+        } else {
+          dispatch(fetchRoomsSuccess(res.data));
+          return res.data;
+        }
+      })
+      .catch((error) => {
+        dispatch(fetchRoomsError(error));
+      });
+  };
+}
+
+function fetchRoomsPending() {
+  return {
+    type: FETCH_ROOMS_PENDING,
+  };
+}
+
+function fetchRoomsSuccess(rooms) {
+  return {
+    type: FETCH_ROOMS_SUCCESS,
+    rooms: rooms,
+  };
+}
+
+function fetchRoomsError(error) {
+  return {
+    type: FETCH_ROOMS_ERROR,
+    error: error,
+  };
+}
+
+function createRoomsSuccess(messages) {
+  return {
+    type: CREATE_ROOMS_SUCCESS,
+    messages: messages,
+  };
+}
+
+function createRoomsError(error) {
+  return {
+    type: CREATE_ROOMS_ERROR,
+    error: error,
+  };
+}
--- Client/src/reducers/index.js
+++ Client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
+import roomReducer from "./roomReducer";
 
 export default combineReducers({
   auth: authReducer,
+  errors: errorReducer,
+  rooms: roomReducer,
 });
